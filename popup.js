0c4a58b945b16efe07849e7a085667819cb66610
Dheeraj Kharwar <100955259+dheerajkharwar@users.noreply.github.com>
2025-03-24 13:54:10 +0530

    speed

---
+++ popup.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("adminIframe").src = "https://tecmicra.co.in/admin/?page=add-new-task&page_type=extension";
+});
